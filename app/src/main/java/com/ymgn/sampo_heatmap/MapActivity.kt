1ef3c4a917886e45bd2e68e2e646ea95c4bcd49f
Hinako Miyoshi <kona.gjpt@gmail.com>
2022-07-24 18:06:33 +0900

    アクティビティかく

--- app/src/main/java/com/ymgn/sampo_heatmap/MapActivity.kt
+++ app/src/main/java/com/ymgn/sampo_heatmap/MapActivity.kt
@@ -1,6 +1,25 @@
 package com.ymgn.sampo_heatmap
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MapActivity: AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mMap: GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // MapFragmentの生成
+        val mapFragment = MapFragment.newInstance()
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
 }
